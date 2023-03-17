8ef8e56b28802faccb1947d84ece7e679d3010d4
AEnyChris <enyonejoseph@gmail.com>
2023-03-17 19:39:31 +0000

    added 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 if successful
+ *
+ */
+int main(void)
+{
+	char *alpha;
+	int i;
+
+	alpha = "abcdefghijklmnopqrstuvwxyz";
+	i = 0;
+	while (alpha[i] != '\0')
+	{
+		putchar(alpha[i]);
+		putchar('\n');
+		i++;
+	}
+	return (0);
+}
