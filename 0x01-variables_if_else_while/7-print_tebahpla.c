df447f6e325d073eea589ee632fef3ed348fa6ea
AEnyChris <enyonejoseph@gmail.com>
2023-03-17 21:30:11 +0000

    added 7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - Entry point
+ *
+ *Return: 0 if successful
+ *
+ */
+int main(void)
+{
+	char *alpha;
+	int i;
+
+	alpha = "zyxwvutsrqponmlkjihgfecba";
+	i = 0;
+	while (alpha[i] != '\0')
+	{
+		putchar(alpha[i]);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
