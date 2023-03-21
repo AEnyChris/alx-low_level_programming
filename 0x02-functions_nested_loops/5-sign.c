452e7960a02bec45bfc17251eb91a8042abc9d3c
AEnyChris <enyonejoseph@gmail.com>
2023-03-21 23:15:41 +0000

    added 5-sign.c and updated main.h with corresponding prototype

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_sign - Entry point
+ *@n: number to be checked
+ *
+ * Description: check and print the sign of a number
+ *
+ * Return: 1 if true. 0 if not
+ */
+
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
