83e6eae8cc0d883d62805d2b12f4a2d673aa16c7
AEnyChris <enyonejoseph@gmail.com>
2023-03-22 01:38:59 +0000

    added 7-print_last_digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * print_last_digit - Entry point
+ *@i: character to be checked
+ *
+ * Description: return the value of the last digit
+ *
+ * Return: last digit of integer
+ */
+
+int print_last_digit(int i)
+{
+	return ((i % 10));
+}
