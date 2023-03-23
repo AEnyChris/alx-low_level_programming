c8fb81315663031ec3160fbdd416e73735d3903f
AEnyChris <enyonejoseph@gmail.com>
2023-03-23 01:21:25 +0000

    added 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - Entry point
+ * @i: first operand
+ * @j: second operand
+ *
+ * Description: adds the two operands
+ *
+ * Return: the sum of the two operands
+ */
+
+int add(int i, int j)
+{
+	return (i + j);
+}
