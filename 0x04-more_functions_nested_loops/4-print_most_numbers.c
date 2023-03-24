3bc8a98bf5c43490dd9addd91de4870b83d1f2df
AEnyChris <enyonejoseph@gmail.com>
2023-03-24 04:21:25 +0000

    added 4-prnt_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <ctype.h>
+
+/**
+ * print_most_numbers - print 0 - 9. without 2 and 4
+ *
+ * Return: void
+ */
+void print_most_numbers(void)
+{
+	int i;
+
+	for (i = '0'; i <= '9'; i++)
+	{
+		if (i != '2' && i != '4')
+			_putchar(i);
+	}
+	_putchar('\n');
+}
