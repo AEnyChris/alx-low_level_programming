ddb643f2d8105312a1f9c553253a503f49924a47
AEnyChris <enyonejoseph@gmail.com>
2023-03-30 22:31:22 +0000

    Initial commit in directory 0x06. Adding README 0-strcat.c and main.h

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <string.h>
+/**
+ * _strcat - concatenate two strings
+ *
+ * @dest: first string
+ * @src: second string
+ *
+ * Return: pointer to result
+ */
+char *_strcat(char *dest, char *src)
+{
+	return (strcat(dest, src));
+}
