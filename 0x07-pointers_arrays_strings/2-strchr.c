ad7f9a59f40356066c60e317d80ebed07e2fd198
AEnyChris <enyonejoseph@gmail.com>
2023-04-03 16:33:35 +0000

    added new file 2-strchr.c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include <string.h>
+
+/**
+ * _strchr - locates a character in a string
+ * @s: string
+ * @c: the character
+ *
+ * Return: pointer to dest
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] > '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (NULL);
+}
