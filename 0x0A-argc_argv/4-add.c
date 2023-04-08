4500f392113b3fe2e605f1b533b2f62f4c95fe72
AEnyChris <enyonejoseph@gmail.com>
2023-04-08 12:18:32 +0000

    added 4-add.c file

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,51 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+/**
+ * main - Entry point of programme
+ * @argc: count of arguments
+ * @argv: vector array of arguments
+ *
+ * Return: int
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, flag;
+	unsigned long int j;
+	int sum = 0;
+
+	if (argc == 1)
+	{
+		printf("%d\n", 0);
+		return (1);
+	}
+	else
+		for (i = 1; i < argc; i++)
+		{
+			for (j = 0; j < strlen(argv[i]); j++)
+			{
+				if (argv[i][j] >= '0' && argv[i][j] <= '9')
+				{
+					flag = 1;
+				}
+				else
+				{
+					flag = 0;
+					break;
+				}
+			}
+			if (flag == 1)
+			{
+				sum += atoi(argv[i]);
+			}
+			else
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+	printf("%d\n", sum);
+	return (0);
+}
