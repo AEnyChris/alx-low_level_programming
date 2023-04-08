c09c0363dde92bc7d41ae13006adb8f9c38d715f
AEnyChris <enyonejoseph@gmail.com>
2023-04-08 10:48:14 +0000

    added 1-args.c

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of programme
+ * @argc: count of arguments
+ * @argv: vector array of arguments
+ *
+ * Return: int
+ */
+
+int main(int argc, char __attribute__((unused)) *argv[])
+{
+	printf("%d\n", argc);
+	return (0);
+}
