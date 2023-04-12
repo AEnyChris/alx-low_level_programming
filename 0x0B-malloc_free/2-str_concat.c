25a858dc16d18d815ae0c899659359326d38417e
AEnyChris <enyonejoseph@gmail.com>
2023-04-12 09:37:20 +0000

    updated 22-str_concat.c to accomodate NULL values as parameters

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,13 +20,8 @@ char *str_concat(char *s1, char *s2)
 	{
 		s1 = "";
 	}
-	else if (s2 == NULL)
+	if (s2 == NULL)
 	{
-		s2 = "";
-	}
-	else if (s1 == NULL && s2 == NULL)
-	{
-		s1 = "";
 		s2 = "";
 	}
 	ar = malloc(sizeof(char) * (strlen(s1) + strlen(s2) + 1));
