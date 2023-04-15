9ea854f19aa1f4196c0f58320eecfadd68e3029b
AEnyChris <enyonejoseph@gmail.com>
2023-04-15 02:26:53 +0000

    correctoin on 1-string_nconcat.c

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -29,7 +29,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		n = strlen(s2);
 	}
-	ar = malloc(sizeof(char) * (strlen(s1) + n + 1));
+	ar = malloc(sizeof(char) * (strlen(s1) + n));
 	if (ar == NULL)
 		return (NULL);
 	for (i = 0; i < strlen(s1); i++)
