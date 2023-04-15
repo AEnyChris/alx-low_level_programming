60fde27ceb9d4b565f012d7e08bfc5128fd9234a
AEnyChris <enyonejoseph@gmail.com>
2023-04-15 02:53:19 +0000

    added 2-calloc.c

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stddef.h>
+
+/**
+ * _calloc - returns a pointer to a copied string
+ * @nmemb: size_t size of memory to be allocated
+ * @size: size of bytes
+ *
+ * Return: pointer to  allocated memory or exit(98)
+ */
+
+void *_calloc(unsigned nmemb, unsigned int size)
+{
+	void *pointer;	
+
+	if (nmemb == 0 || size == 0)
+	{
+		return ('\0');
+	}
+
+	pointer = malloc(size * nmemb);
+	if (pointer == NULL)
+		return (NULL);
+	memset(pointer, 0, nmemb * size);
+
+	return (pointer);
+}
