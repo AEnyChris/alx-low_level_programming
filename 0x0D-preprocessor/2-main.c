6ce0f95221ace7b4af98c496f208c763e22df887
AEnyChris <enyonejoseph@gmail.com>
2023-04-17 19:49:11 +0000

    added 2-main.c

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Description: function to print current filename
+ *
+ * Return: void
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	
+	return (0);
+}
