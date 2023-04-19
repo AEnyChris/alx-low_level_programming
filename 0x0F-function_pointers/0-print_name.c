fde045e104f9ca604e59a2e2398bab25ad5b4d55
AEnyChris <enyonejoseph@gmail.com>
2023-04-19 12:45:48 +0000

    correction

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,4 +1,4 @@
-
+#include <stddef.h>
 /**
  * print_name - a function that prints a name
  * @name: the name to be printed
@@ -9,5 +9,7 @@
 
 void print_name(char *name, void (*f)(char *))
 {
+	if (f == NULL || name == NULL)
+		return;
 	f(name);
 }
