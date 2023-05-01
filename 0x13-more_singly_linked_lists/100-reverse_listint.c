4a903923c688d733b4e495170a457bd01ac6831e
AEnyChris <enyonejoseph@gmail.com>
2023-05-01 19:56:04 +0000

    added 10 and 11

---
+++ 0x13-more_singly_linked_lists/100-reverse_listint.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include "lists.h"
+#include <stddef.h>
+
+/**
+ * reverse_listint- inserts a new node at given position
+ * @head: pointer to pointer to head
+ *
+ * Return: address of first node of reversed list
+ */
+listint_t *reverse_listint(listint_t **head)
+{
+	listint_t *temp;
+	listint_t *safe;
+
+	if (head == NULL)
+		return (NULL);
+
+	temp = *head;
+	safe = (temp->next)->next; /*keeping N2 safe*/
+	(temp->next)->next = *head;
+	*head = temp->next;
+	temp->next = NULL;
+	while (safe)
+	{
+		temp = safe;
+		safe = temp->next;
+		temp->next = *head;
+		*head = temp;
+	}
+	return (*head);
+}
