07072d404864571d225a02d239dfb751de717b9c
Enyone Christian Achobe <enyonejoseph@gmail.com>
2024-07-17 23:20:32 +0100

    binary search aglo

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -28,7 +28,11 @@ int binary_search(int *array, size_t size, int value)
 	{
 		printf("Searching in array: ");
 		for (i = L; i <= R; i++)
-			printf("%d, ", array[i]);
+		{
+			printf("%d", array[i]);
+			if (i < R)
+				printf(", ");
+		}
 		printf("\n");
 
 		m = (R + L) / 2;
